29f36b1fae3595e3e196d104084b2ee52e965131
kungsovannda <kungsovannda@gmail.com>
2026-03-01 04:16:15 +0700

    add: transfer request

---
+++ microservices/wallet-service/command/src/main/java/co/istad/wallet/command/interfaces/dto/TransferMoneyRequestDto.java
@@ -0,0 +1,10 @@
+package co.istad.wallet.command.interfaces.dto;
+
+import co.istad.wallet.common.vo.Money;
+import co.istad.wallet.common.vo.WalletId;
+
+public record TransferMoneyRequestDto(
+        Money amount,
+        WalletId toWalletId
+) {
+}
