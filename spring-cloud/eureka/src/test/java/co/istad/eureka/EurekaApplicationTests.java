76f920dc31804762cc4d0af0c161727b281d087f
kungsovannda <kungsovannda@gmail.com>
2026-03-01 04:21:37 +0700

    init: eureka with configuration

---
+++ spring-cloud/eureka/src/test/java/co/istad/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.istad.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
