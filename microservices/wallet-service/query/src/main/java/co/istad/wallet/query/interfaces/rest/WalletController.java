afd80c2e773e3b9458f5354bc4056298ab0186e2
kungsovannda <kungsovannda@gmail.com>
2026-03-01 04:17:45 +0700

    feat: implement wallet controller for query wallet detail by its id

---
+++ microservices/wallet-service/query/src/main/java/co/istad/wallet/query/interfaces/rest/WalletController.java
@@ -0,0 +1,32 @@
+package co.istad.wallet.query.interfaces.rest;
+
+import co.istad.wallet.common.vo.WalletId;
+import co.istad.wallet.query.interfaces.dto.WalletResponseDto;
+import co.istad.wallet.query.query.GetWalletQuery;
+import lombok.RequiredArgsConstructor;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
+import org.axonframework.queryhandling.QueryGateway;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/wallets")
+@RequiredArgsConstructor
+public class WalletController {
+
+    private final QueryGateway queryGateway;
+
+    @GetMapping("/{walletId}")
+    public WalletResponseDto getWallet(@PathVariable UUID walletId){
+        GetWalletQuery getWalletQuery = new GetWalletQuery(new WalletId(walletId));
+        return queryGateway.query(getWalletQuery, ResponseTypes.instanceOf(WalletResponseDto.class)).join();
+
+    }
+
+
+
+}
