acc2f471210e303b595b34397e125503a1790f6e
kungsovannda <kungsovannda@gmail.com>
2026-03-01 01:19:51 +0700

    add: deposit money command

---
+++ microservices/wallet-service/command/src/main/java/co/istad/wallet/command/domain/command/DepositMoneyCommand.java
@@ -0,0 +1,12 @@
+package co.istad.wallet.command.domain.command;
+
+import co.istad.wallet.common.vo.WalletId;
+import co.istad.wallet.common.vo.Money;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+public record DepositMoneyCommand(
+        @TargetAggregateIdentifier
+        WalletId walletId,
+        Money amount
+) {
+}
