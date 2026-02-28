0c56539aae4f1b29b79ba2d1bca710573eaea2d8
kungsovannda <kungsovannda@gmail.com>
2026-03-01 01:27:50 +0700

    feat: define attributes of transition view

---
+++ microservices/wallet-service/query/src/main/java/co/istad/wallet/query/view/TransactionView.java
@@ -0,0 +1,28 @@
+package co.istad.wallet.query.view;
+
+import co.istad.wallet.common.vo.Money;
+import co.istad.wallet.query.vo.TransactionType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Document(collection = "transactions")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TransactionView {
+    @Id
+    private String transactionId;
+    private String walletId;
+    private Money amount;
+    private TransactionType type;
+    private String transferId;
+    private String counterpartWalletId;
+    private Instant timestamp;
+}
