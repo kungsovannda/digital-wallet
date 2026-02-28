6658eb338d01f42744e8ea7d8a3cf0908b999fde
kungsovannda <kungsovannda@gmail.com>
2026-03-01 04:16:34 +0700

    add: deposit request

---
+++ microservices/wallet-service/command/src/main/java/co/istad/wallet/command/interfaces/dto/DepositMoneyRequestDto.java
@@ -0,0 +1,9 @@
+package co.istad.wallet.command.interfaces.dto;
+
+import co.istad.wallet.common.vo.Money;
+import co.istad.wallet.common.vo.WalletId;
+
+public record DepositMoneyRequestDto(
+        Money amount
+) {
+}
