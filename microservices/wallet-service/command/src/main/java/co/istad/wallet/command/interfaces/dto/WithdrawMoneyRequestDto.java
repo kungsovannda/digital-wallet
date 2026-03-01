3d110e14b924a54f220e741c60e033dd3c379d4b
kungsovannda <kungsovannda@gmail.com>
2026-03-01 12:16:46 +0700

    add: validation to dto

--- microservices/wallet-service/command/src/main/java/co/istad/wallet/command/interfaces/dto/WithdrawMoneyRequestDto.java
+++ microservices/wallet-service/command/src/main/java/co/istad/wallet/command/interfaces/dto/WithdrawMoneyRequestDto.java
@@ -1,9 +1,10 @@
 package co.istad.wallet.command.interfaces.dto;
 
 import co.istad.wallet.common.vo.Money;
-import co.istad.wallet.common.vo.WalletId;
+import jakarta.validation.constraints.NotNull;
 
 public record WithdrawMoneyRequestDto(
+        @NotNull(message = "Amount is required")
         Money amount
 ) {
 }
