e2a607ef3fa9756e09c822576cccda6b3d7bf5f9
kungsovannda <kungsovannda@gmail.com>
2026-03-01 12:18:04 +0700

    change: generate uuid on command

--- microservices/wallet-service/command/src/main/java/co/istad/wallet/command/saga/TransferSaga.java
+++ microservices/wallet-service/command/src/main/java/co/istad/wallet/command/saga/TransferSaga.java
@@ -2,12 +2,15 @@ package co.istad.wallet.command.saga;
 
 import co.istad.wallet.command.domain.command.CreditMoneyCommand;
 import co.istad.wallet.common.event.MoneyDebitedEvent;
+import co.istad.wallet.common.vo.TransactionId;
 import lombok.NoArgsConstructor;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.modelling.saga.SagaEventHandler;
 import org.axonframework.modelling.saga.StartSaga;
 import org.axonframework.spring.stereotype.Saga;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
 
 @Saga
 @NoArgsConstructor
@@ -20,6 +23,7 @@ public class TransferSaga {
                 new CreditMoneyCommand(
                         event.toWalletId(),
                         event.walletId(),
+                        new TransactionId(UUID.randomUUID()),
                         event.transferId(),
                         event.amount()
                 )
