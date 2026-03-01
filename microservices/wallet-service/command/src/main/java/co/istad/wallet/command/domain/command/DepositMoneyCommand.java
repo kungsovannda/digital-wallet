09437eb61a239bb445933b072182bcfc7b27831e
kungsovannda <kungsovannda@gmail.com>
2026-03-01 12:17:12 +0700

    change: uuid generation in command instead of aggregate

--- microservices/wallet-service/command/src/main/java/co/istad/wallet/command/domain/command/DepositMoneyCommand.java
+++ microservices/wallet-service/command/src/main/java/co/istad/wallet/command/domain/command/DepositMoneyCommand.java
@@ -1,5 +1,6 @@
 package co.istad.wallet.command.domain.command;
 
+import co.istad.wallet.common.vo.TransactionId;
 import co.istad.wallet.common.vo.WalletId;
 import co.istad.wallet.common.vo.Money;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
@@ -7,6 +8,7 @@ import org.axonframework.modelling.command.TargetAggregateIdentifier;
 public record DepositMoneyCommand(
         @TargetAggregateIdentifier
         WalletId walletId,
+        TransactionId transactionId,
         Money amount
 ) {
 }
