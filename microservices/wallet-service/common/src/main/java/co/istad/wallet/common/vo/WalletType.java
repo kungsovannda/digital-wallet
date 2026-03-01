899730dd4418ffd0d115741929103efa4844a8ff
kungsovannda <kungsovannda@gmail.com>
2026-03-01 12:20:26 +0700

    change: introduce daily limit rate in wallet type

--- microservices/wallet-service/common/src/main/java/co/istad/wallet/common/vo/WalletType.java
+++ microservices/wallet-service/common/src/main/java/co/istad/wallet/common/vo/WalletType.java
@@ -1,7 +1,15 @@
 package co.istad.wallet.common.vo;
 
+import java.math.BigDecimal;
+
 public enum WalletType {
-    STANDARD,
-    PREMIUM,
-    BUSINESS
+    STANDARD(new BigDecimal("1000")),
+    PREMIUM(new BigDecimal("5000")),
+    BUSINESS(new BigDecimal("10000"));
+
+    private final BigDecimal dailyLimit;
+
+    WalletType(BigDecimal dailyLimit) { this.dailyLimit = dailyLimit; }
+
+    public BigDecimal getDailyLimit() { return dailyLimit; }
 }
