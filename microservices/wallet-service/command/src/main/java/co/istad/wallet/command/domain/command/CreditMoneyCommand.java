5f8c05a4ebe58657dbf3fd7e9a7c230258ab624b
kungsovannda <kungsovannda@gmail.com>
2026-03-01 12:17:09 +0700

    change: uuid generation in command instead of aggregate

--- microservices/wallet-service/command/src/main/java/co/istad/wallet/command/domain/command/CreditMoneyCommand.java
+++ microservices/wallet-service/command/src/main/java/co/istad/wallet/command/domain/command/CreditMoneyCommand.java
@@ -1,5 +1,6 @@
 package co.istad.wallet.command.domain.command;
 
+import co.istad.wallet.common.vo.TransactionId;
 import co.istad.wallet.common.vo.WalletId;
 import co.istad.wallet.common.vo.TransferId;
 import co.istad.wallet.common.vo.Money;
@@ -9,6 +10,7 @@ public record CreditMoneyCommand(
         @TargetAggregateIdentifier
         WalletId walletId,
         WalletId fromWalletId,
+        TransactionId transactionId,
         TransferId transferId,
         Money amount
 ) {
