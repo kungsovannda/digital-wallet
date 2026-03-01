363dfc83980b73c3f8ad0d178e5ce3fea80334ba
kungsovannda <kungsovannda@gmail.com>
2026-03-01 12:16:34 +0700

    add: validation to dto

--- microservices/wallet-service/command/src/main/java/co/istad/wallet/command/interfaces/dto/CreateWalletRequestDto.java
+++ microservices/wallet-service/command/src/main/java/co/istad/wallet/command/interfaces/dto/CreateWalletRequestDto.java
@@ -2,11 +2,14 @@ package co.istad.wallet.command.interfaces.dto;
 
 import co.istad.wallet.common.vo.Money;
 import co.istad.wallet.common.vo.UserId;
-import co.istad.wallet.common.vo.WalletId;
 import co.istad.wallet.common.vo.WalletType;
+import jakarta.validation.constraints.NotNull;
 
 public record CreateWalletRequestDto(
+        @NotNull(message = "Owner id is required")
         UserId ownerId,
+        @NotNull(message = "Balance is required")
         Money balance,
+        @NotNull(message = "Wallet type is required")
         WalletType type) {
 }
